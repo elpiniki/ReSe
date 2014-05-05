1faa3157177784b70d04471b7a9bd13f78b7715e
Nikos Vasilakis <nikos.ailo@gmail.com>
2014-05-04 20:52:19 -0400

    Connecting DB

--- web/app.js
+++ web/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var db = require('./db/final.json');
 
 app.get('/hello.txt', function(req, res){
     res.send('Hello World');
@@ -10,15 +11,19 @@ app.get('/', function(req, res){
 });
 
 app.get('/getResults', function(req, res){
+  console.log("[Elpiniki's Server] Received term: " + req.query.term);
+  var results = db[req.query.term];
+  // grafeis ton algori8mo sou: apo ta results => sto 1 Result
+  var result = results[0];
   qresults = {
-                "title" : "University of Delaware | V2G"
-            ,   "score" : "12.32"
-            ,   "link"  : "http://www.udel.edu/v2g"
+                "title" : result.doc
+            ,   "score" : result.tf
+            ,   "link"  : result.doc
             };
   res.set('Access-Control-Allow-Origin', '*');
   res.json(qresults);
 })
 ;
 var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+    console.log("[Elpiniki's Server] Listening on port %d", server.address().port);
 });
