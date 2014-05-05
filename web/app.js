00f5fd43687620237474dad5379111573ad10248
Nikos Vasilakis <nikos.ailo@gmail.com>
2014-05-04 20:37:01 -0400

    Start of server side

--- web/app.js
+++ web/app.js
@@ -9,6 +9,16 @@ app.get('/', function(req, res){
     res.send('Tipota');
 });
 
+app.get('/getResults', function(req, res){
+  qresults = {
+                "title" : "University of Delaware | V2G"
+            ,   "score" : "12.32"
+            ,   "link"  : "http://www.udel.edu/v2g"
+            };
+  res.set('Access-Control-Allow-Origin', '*');
+  res.json(qresults);
+})
+;
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
